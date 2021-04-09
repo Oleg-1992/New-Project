5ee4b86f4b5a7d301dec86b2b3c4df6d3e7eeb0e
Oleg-1992 <34855361+Oleg-1992@users.noreply.github.com>
2021-04-09 16:07:10 +0300

    Адаптация

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,  
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },  
 
   // Navigation arrows
   navigation: {
@@ -14,7 +32,16 @@ const swiper = new Swiper('.channel-slider', {
 const recommended = new Swiper('.recommended-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,  
+  slidesPerView: 1, 
+  spaceBetween: 20,
+  breakpoints: {
+  1600: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    },
+  }, 
 
   // Navigation arrows
   navigation: {
@@ -27,7 +54,25 @@ const recommended = new Swiper('.recommended-slider', {
 const food = new Swiper('.food-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,  
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },   
 
   // Navigation arrows
   navigation: {
@@ -35,3 +80,15 @@ const food = new Swiper('.food-slider', {
     prevEl: '.food-button-prev',    
   },  
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  recommended.destroy();
+  food.destroy();
+
+}
